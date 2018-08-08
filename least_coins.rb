1a3cc1c0588a799396325cc29ca5ea6bb56c0807
Abigail Slovick <abby.slovick@gmail.com>
2018-08-08 21:18:36 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -45,37 +45,6 @@
 
 # least_coins(29)
 
-# def least_coins(cents)
-#   coins = {
-#     :quarters => 0,
-#     :dimes => 0,
-#     :nickels => 0,
-#     :pennies => 0
-#   }
-
-#   # counter = 0
-
-#   until cents == 0
-#     if cents >= 25
-#         coins[:quarters] += 1
-#         cents = cents - 25
-#     elsif cents >= 10
-#         coins[:dimes] += 1
-#         cents = cents - 10
-#     elsif cents >= 5
-#         coins[:nickels] += 1
-#         cents = cents - 5
-#     elsif cents >= 1
-#         coins[:pennies] += 1
-#         cents = cents - 1
-#     else
-#       break
-#     end
-#   end
-#   puts coins
-# end
-# least_coins(52)
-
 def least_coins(cents)
   coins = {
     :quarters => 0,
@@ -83,24 +52,55 @@ def least_coins(cents)
     :nickels => 0,
     :pennies => 0
   }
-  
+
+  # counter = 0
+
   until cents == 0
-  if cents % 25 == 0
-    coins[:quarters] += 1
-    cents = cents - 25
-  elsif cents % 10 == 0
-    coins[:dimes] += 1
-    cents = cents - 10
-  elsif cents % 5 == 0
-    coins[:nickels] += 1
-    cents = cents - 5
-  elsif cents % 1 == 0 
-    coins[:pennies] += 1
-    cents = cents - 1
-  else
-    break
-  end
+    if cents >= 25
+        coins[:quarters] += 1
+        cents = cents - 25
+    elsif cents >= 10
+        coins[:dimes] += 1
+        cents = cents - 10
+    elsif cents >= 5
+        coins[:nickels] += 1
+        cents = cents - 5
+    elsif cents >= 1
+        coins[:pennies] += 1
+        cents = cents - 1
+    else
+      break
+    end
   end
   puts coins
 end
-least_coins(130)
+least_coins(52)
+
+# def least_coins(cents)
+#   coins = {
+#     :quarters => 0,
+#     :dimes => 0,
+#     :nickels => 0,
+#     :pennies => 0
+#   }
+  
+#   until cents == 0
+#   if cents % 25 == 0
+#     coins[:quarters] += 1
+#     cents = cents - 25
+#   elsif cents % 10 == 0
+#     coins[:dimes] += 1
+#     cents = cents - 10
+#   elsif cents % 5 == 0
+#     coins[:nickels] += 1
+#     cents = cents - 5
+#   elsif cents % 1 == 0 
+#     coins[:pennies] += 1
+#     cents = cents - 1
+#   else
+#     break
+#   end
+#   end
+#   puts coins
+# end
+# least_coins(130)
