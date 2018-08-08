0e68efd985c7a96397d3a7f7d06c4623600487e1
Abigail Slovick <abby.slovick@gmail.com>
2018-08-08 20:54:31 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,45 +1,78 @@
+# def least_coins(cents)
+#   coin_types = {
+#     :quarters => 0,
+#     :dimes => 0,
+#     :nickels => 0,
+#     :pennies => 0
+#   }
+
+#   quarters = cents/25
+#   if quarters == 0
+#     coin_types[:quarters] = 0
+#   elsif quarters != 0
+#     coin_types[:quarters] = quarters
+#     cents = cents - quarters*25
+#   end
+
+#   dimes = cents/10
+#   if dimes == 0
+#     coin_types[:dimes] = 0
+#   elsif dimes != 0
+#     coin_types[:dimes] = dimes
+#     cents = cents - dimes*10
+#   end
+
+#   nickels = cents/5
+#   if nickels == 0
+#     coin_types[:nickels] = 0
+#   elsif nickels != 0
+#     coin_types[:nickels] = nickels
+#     cents = cents - nickels*5
+#   end
+
+#   pennies = cents
+#   if pennies == 0
+#     coin_types[:pennies] = 0
+#   elsif pennies != 0
+#     coin_types[:pennies] = pennies
+#   end
+
+#   puts coin_types
+
+#   coin_types
+
+# end
+
+# least_coins(29)
+
 def least_coins(cents)
-  coin_types = {
+
+  coins = {
     :quarters => 0,
     :dimes => 0,
     :nickels => 0,
     :pennies => 0
   }
 
-  quarters = cents/25
-  if quarters == 0
-    coin_types[:quarters] = 0
-  elsif quarters != 0
-    coin_types[:quarters] = quarters
-    cents = cents - quarters*25
+  counter = 0
+
+  until cents == 0
+    if cents >= 25
+        coins[:quarters] += 1
+        cents = cents - 25
+    elsif cents >= 10
+        coins[:dimes] += 1
+        cents = cents - 10
+    elsif cents >= 5
+        coins[:nickels] += 1
+        cents = cents - 5
+    elsif cents >= 1
+        coins[:pennies] += 1
+        cents = cents - 1
+    else
+      break
+    end
   end
-
-  dimes = cents/10
-  if dimes == 0
-    coin_types[:dimes] = 0
-  elsif dimes != 0
-    coin_types[:dimes] = dimes
-    cents = cents - dimes*10
-  end
-
-  nickels = cents/5
-  if nickels == 0
-    coin_types[:nickels] = 0
-  elsif nickels != 0
-    coin_types[:nickels] = nickels
-    cents = cents - nickels*5
-  end
-
-  pennies = cents
-  if pennies == 0
-    coin_types[:pennies] = 0
-  elsif pennies != 0
-    coin_types[:pennies] = pennies
-  end
-
-  puts coin_types
-
-  coin_types
-
+  coins
 end
-
+least_coins(52)
