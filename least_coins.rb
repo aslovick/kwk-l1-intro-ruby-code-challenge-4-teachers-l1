4da9d41d19ee6d0f66704e7ee0e651addf65ba0e
Abigail Slovick <abby.slovick@gmail.com>
2018-08-08 21:01:43 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -45,36 +45,36 @@
 
 # least_coins(29)
 
-def least_coins(cents)
-  coins = {
-    :quarters => 0,
-    :dimes => 0,
-    :nickels => 0,
-    :pennies => 0
-  }
+# def least_coins(cents)
+#   coins = {
+#     :quarters => 0,
+#     :dimes => 0,
+#     :nickels => 0,
+#     :pennies => 0
+#   }
 
-  # counter = 0
+#   # counter = 0
 
-  until cents == 0
-    if cents >= 25
-        coins[:quarters] += 1
-        cents = cents - 25
-    elsif cents >= 10
-        coins[:dimes] += 1
-        cents = cents - 10
-    elsif cents >= 5
-        coins[:nickels] += 1
-        cents = cents - 5
-    elsif cents >= 1
-        coins[:pennies] += 1
-        cents = cents - 1
-    else
-      break
-    end
-  end
-  puts coins
-end
-least_coins(52)
+#   until cents == 0
+#     if cents >= 25
+#         coins[:quarters] += 1
+#         cents = cents - 25
+#     elsif cents >= 10
+#         coins[:dimes] += 1
+#         cents = cents - 10
+#     elsif cents >= 5
+#         coins[:nickels] += 1
+#         cents = cents - 5
+#     elsif cents >= 1
+#         coins[:pennies] += 1
+#         cents = cents - 1
+#     else
+#       break
+#     end
+#   end
+#   puts coins
+# end
+# least_coins(52)
 
 def least_coins(cents)
   coins = {
@@ -104,4 +104,3 @@ def least_coins(cents)
   puts coins
 end
 least_coins(52)
-end 
