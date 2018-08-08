e0e26544e06c07d8c1bc41bd22170f4467e0e7a8
Abigail Slovick <abby.slovick@gmail.com>
2018-08-08 21:01:32 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -68,10 +68,10 @@ def least_coins(cents)
     elsif cents >= 1
         coins[:pennies] += 1
         cents = cents - 1
-    # else
-      # break
+    else
+      break
     end
-  # end
+  end
   puts coins
 end
 least_coins(52)
@@ -83,4 +83,25 @@ def least_coins(cents)
     :nickels => 0,
     :pennies => 0
   }
+  
+  until cents == 0
+  if cents % 25 == 0
+    coins[:quarters] += 1
+    cents = cents - 25
+  elsif cents % 10 == 0
+    coins[:dimes] += 1
+    cents = cents - 10
+  elsif cents % 5 == 0
+    coins[:nickels] += 1
+    cents = cents - 5
+  elsif cents % 1 == 0 
+    coins[:pennies] += 1
+    cents = cents - 1
+  else
+    break
+  end
+  end
+  puts coins
+end
+least_coins(52)
 end 
